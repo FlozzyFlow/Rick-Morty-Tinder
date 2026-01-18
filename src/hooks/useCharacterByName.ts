5e5f4067913a14228caabeb2141e92b49a4a4a99
flozzy <flozzy@gmail.com>
2026-01-18 10:27:51 +0300

    final build

--- src/hooks/useCharacterByName.ts
+++ src/hooks/useCharacterByName.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getCharacterByName } from "../api/Api";
 
 const useCharacterByName = (name: string) => {
